466e6885607ea2b1b85d63c1e2646b758ce4c52a
George Kankava <george.kankava@aurea.com>
2017-07-09 13:23:28 +0400

    Comparator Example Java 8.

--- src/main/java/com/java8/lambdas/ComparatorExample.java
+++ src/main/java/com/java8/lambdas/ComparatorExample.java
@@ -1,7 +1,19 @@
 package com.java8.lambdas;
 
+import java.util.Comparator;
+
 /**
  * Created by georgekankava on 09.07.17.
  */
 public class ComparatorExample {
+    public static void main(String[] args) {
+        // Lambda expression
+        Comparator<Apple> appleComparator = Comparator.comparing((apple -> apple.getWeight()));
+        // Method reference expression
+        appleComparator = Comparator.comparing(Apple::getWeight);
+    }
+
+    public interface Apple {
+        int getWeight();
+    }
 }
