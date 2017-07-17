b40eb12d93d58e599f72caa19f8a07676b041a51
George Kankava <george.kankava@aurea.com>
2017-07-17 11:54:15 +0400

    Working with steams.

--- src/main/java/com/java8/lambdas/CollectorReducingExample.java
+++ src/main/java/com/java8/lambdas/CollectorReducingExample.java
@@ -1,6 +1,8 @@
 package com.java8.lambdas;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
@@ -11,5 +13,9 @@ public class CollectorReducingExample {
         Stream<Integer> integerStream = Stream.of(1, 2, 3, 4, 5, 6, 7);
         Optional<Integer> reduce = integerStream.reduce((integer, integer2) -> integer > integer2 ? integer : integer2);
         System.out.println(reduce.orElse(0));
+
+        Stream
+                .of(1, 21)
+                .collect(Collectors.reducing((o, o2) -> Integer.max(o, o2)));
     }
 }
