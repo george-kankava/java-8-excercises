b1dba46502669ccc75a320430ca96f7a1a3b678a
George Kankava <gkankava@uplandsoftware.com>
2024-02-18 16:19:12 +0400

    class initializer

--- src/main/java/com/java8/collectors/CollectorExamples.java
+++ src/main/java/com/java8/collectors/CollectorExamples.java
@@ -1,12 +1,19 @@
 package com.java8.collectors;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
 
+import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.maxBy;
+import static java.util.stream.Collectors.partitioningBy;
+import static java.util.stream.Collectors.toList;
 
 public class CollectorExamples {
 
@@ -35,7 +42,29 @@ public class CollectorExamples {
                 .collect(groupingBy(Dish::type, maxBy(Comparator.comparingInt(Dish::calories))));
         System.out.println(dishTypeLongMap);
 
+        Map<Boolean, Dish> booleanDishMap = dishes.stream()
+                .collect(partitioningBy(dish -> dish.type() == DishType.FISH || dish.type() == DishType.MEAT,
+                        collectingAndThen(maxBy(Comparator.comparingInt(Dish::calories)), Optional::get)));
+        System.out.println(booleanDishMap);
+        Map<String, String> hashMap = new HashMap<>() {
+            {
+                System.out.println("Created");
+                put("test", "testvalue");
+                System.out.println(get("test"));
+            }
+            @Override
+            public boolean isEmpty() {
+                return super.isEmpty();
+            }
+        };
 
+
+
+    }
+
+    public Supplier<Map<Boolean, List<Integer>>> supplier() { return () -> new HashMap<Boolean, List<Integer>>() {{
+        put(true, new ArrayList<Integer>());
+        put(false, new ArrayList<Integer>()); }};
     }
 
     public enum DishType {
