737cdf69c1674b129a568b12589c0a9f9ee62c8e
George Kankava <gkankava@uplandsoftware.com>
2024-02-28 16:40:36 +0400

    class initializer

--- src/main/java/com/java8/streams/ProcessingStream.java
+++ src/main/java/com/java8/streams/ProcessingStream.java
@@ -3,7 +3,10 @@ package com.java8.streams;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 public class ProcessingStream {
 
@@ -21,5 +24,32 @@ public class ProcessingStream {
                 })
                 .collect(Collectors.toList());
         System.out.println(collect);
+
+        // removed sout test1 branch
+
+        Optional<Integer> sum = Stream.iterate(1, i -> i + 1)
+                .limit(5)
+                .parallel()
+                .reduce(Integer::sum);
+
+        System.out.println(sum.orElse(1));
+
+
+        System.getenv();
+        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "12");
+
+
+        System.out.println("number of processors : " + Runtime.getRuntime().availableProcessors());
+
+        long start = System.currentTimeMillis();
+
+        Long longSum = LongStream.rangeClosed(0, 100)
+                .parallel()
+                .reduce(Long::sum)
+                .orElse(0);
+
+        long end = System.currentTimeMillis() - start;
+        System.out.println("reduce(Long::sum) time : " + end);
+        System.out.println(longSum);
     }
 }
